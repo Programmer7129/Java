2380c292f1974bbc7f3320adbdf719ba4e41d859
Vedant Patel <77016049+Programmer7129@users.noreply.github.com>
2021-05-08 16:24:19 +0530

    Not Complete Programs

--- lesson1/Interface.java
+++
@@ -1,8 +0,0 @@
-package lesson1;
-
-public interface Interface {  // If we use interface keyword than we don't ned to use abstract
-	                          // And all the instances are abstract by nature in interface.
-	int getInterestRate();
-
-
-}
--- lesson1/Interface_ABC.java
+++
@@ -1,8 +0,0 @@
-package lesson1;
-
-public class Interface_ABC implements Interface { // We use keyword 'implements' to inherit from interface
-	public int getInterestRate() {
-		return 5;
-	}
-
-}
